4f9c2d46107474d65c428c52d85af6e9f8d6025c
Mel Booth <mel.m.booth@gmail.com>
2017-02-13 14:03:39 +1300

    sign up form populating - hallelujah!

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 document.getElementById('signup').addEventListener('click', function(e) {
-console.log('Sign Up tab clicked!');
+  console.log('Sign Up tab clicked!')
+  var inputs =
+  document.querySelectorAll('.form-input')
+  document.querySelectorAll('.tabs > li')[0].className=''
+  document.querySelectorAll('.tabs > li')[1].className='active'
+
+  for (var a = 0; a < inputs.length; a++) {
+    if(a == 2){
+
+    } else {
+      document.querySelectorAll('.form-input')[a].className='form-input block'
+    }
+  }
+
+  setTimeout(function() {
+    for(var b = 0; b <inputs.length; b++) {
+      document.querySelectorAll('.form-input')[b].className='form-input block active_input'
+    }
+  }, 100 )
+  document.querySelector('.log-sign-btn').innerHTML = 'Sign Up'
 
 })
 
