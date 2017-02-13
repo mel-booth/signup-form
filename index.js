250354bca8efc6ff953f7c793f8ea92c43a89be3
Mel Booth <mel.m.booth@gmail.com>
2017-02-13 14:46:54 +1300

    log in tab populating

--- index.js
+++ index.js
@@ -23,6 +23,21 @@ document.getElementById('signup').addEventListener('click', function(e) {
 })
 
 document.getElementById('login').addEventListener('click', function(e) {
-console.log('Log In tab clicked!');
+  console.log('Log In tab clicked!')
 
+  var inputs =
+  document.querySelectorAll('.form-input')
+  document.querySelectorAll('.tabs > li')[0].className='active'
+  document.querySelectorAll('.tabs > li')[1].className=''
+
+  for (var a = 0; a < inputs.length; a++) {
+        document.querySelectorAll('.form-input')[a].className='form-input block'
+  }
+
+  setTimeout (function() {
+        document.querySelectorAll('.form-input')[0].className='form-input block active_input'
+        document.querySelectorAll('.form-input')[1].className='form-input block active_input'
+    }, 100 )
+
+  document.querySelector('.log-sign-btn').innerHTML='Log In'
 })
